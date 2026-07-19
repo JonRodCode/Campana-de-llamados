18baeee3625775e40020fe01fdbf1c0c583da044
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2026-07-18 23:41:03 -0300

    Avance en servicio y acceso a datos

---
+++ src/main/java/service/CampaignService.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.Campaign;
+import java.util.List;
+
+public interface CampaignService {
+
+    void createCampaign(Campaign campaign);
+    List<Campaign> getAllCampaigns();
+    Campaign getCampaign(int id);
+    void updateCampaign(Campaign campaign);
+    void deleteCampaign(int id);
+}
---
+++ src/main/java/service/CampaignServiceImpl.java
@@ -0,0 +1,25 @@
+package service;
+
+import model.Campaign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CampaignServiceImpl implements CampaignService {
+
+    public void createCampaign(Campaign campaign){
+
+    }
+    public List<Campaign> getAllCampaigns(){
+
+    }
+    public Campaign getCampaign(int id){
+
+    }
+    public void updateCampaign(Campaign campaign){
+
+    }
+    public void deleteCampaign(int id){
+
+    }
+}
